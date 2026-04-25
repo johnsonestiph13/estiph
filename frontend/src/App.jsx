d4b40c4eb1cb0d3bd3ef3eff2f8545015eb25087
johnsonestiph13 <johnsonestiph13@gmail.com>
2026-04-25 14:00:38 +0300

    jhgfd

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>🏠 Estif Home Ultimate</h1>
+      <p>Loading...</p>
+    </div>
+  );
+}
+
+export default App;
